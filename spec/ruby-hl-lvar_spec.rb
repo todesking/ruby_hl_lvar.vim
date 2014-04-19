9831960aaf638bbbc7029d194475a8253b3e9b30
todesking <discommunicative@gmail.com>
2014-04-19 17:51:42 +0900

    spec: bare lvar ref in top level

--- spec/ruby-hl-lvar_spec.rb
+++ spec/ruby-hl-lvar_spec.rb
@@ -14,50 +14,51 @@ describe RubyHlLvar::Extractor do
       etor.extract('').should be_empty
     end
 
-    context "with top level" do
-      context "with simple assignment" do
-        it { 'a = 1'.should_extract_to [["a", 1, 0]] }
-      end
-      context "with simple mass assignment" do
-        it {
-          'a, b, c = foo'.should_extract_to [["a", 1, 0], ["b", 1, 3], ["c", 1, 6] ]
-        }
-      end
-
-      context "with complex mass assignment" do
-        it { '(a, (b, c)), d = foo'.should_extract_to [["a", 1, 1], ["b", 1, 5], ["c", 1, 8], ["d", 1, 13]] }
-      end
-
-      context "with simple block({ ... }) parameter like {|a, b| }" do
-        it { "foo {|a, b| }".should_extract_to [["a", 1, 6], ["b", 1, 9]] }
-      end
-      context "with simple block({ ... }) without args" do
-        it { "foo { }".should_extract_to [] }
-      end
-      context "with simple block(do ... end) without args" do
-        it { "foo { }".should_extract_to [] }
-      end
-      context "with simple block(do ... end) parameter" do
-        it { "foo do|a|; end".should_extract_to [["a", 1, 7]] }
-      end
-      context "with lvar reference in block body" do
-        it { "foo {|x| x}".should_extract_to [["x", 1, 6], ["x", 1, 9]] }
-      end
-
-      context "bare lvar reference"
-      context "lvar reference in method call lhs"
-      context "lvar reference in method call argument"
-      context "lvar reference in block"
-      context "lvar reference in method body"
-      context "lvar reference in method class body"
-      context "lvar reference in method module body"
-      context "method reference in expr should ignored"
-
-      it "with complex block parameter like {|a, (b, c)| }"
-      it "with multi assignment like a = b = c"
-      it "with assignment in rhs"
-      it "with assignment in method call"
+    context "with simple assignment" do
+      it { 'a = 1'.should_extract_to [["a", 1, 0]] }
     end
+    context "with simple mass assignment" do
+      it {
+        'a, b, c = foo'.should_extract_to [["a", 1, 0], ["b", 1, 3], ["c", 1, 6] ]
+      }
+    end
+
+    context "with complex mass assignment" do
+      it { '(a, (b, c)), d = foo'.should_extract_to [["a", 1, 1], ["b", 1, 5], ["c", 1, 8], ["d", 1, 13]] }
+    end
+
+    context "with simple block({ ... }) parameter like {|a, b| }" do
+      it { "foo {|a, b| }".should_extract_to [["a", 1, 6], ["b", 1, 9]] }
+    end
+    context "with simple block({ ... }) without args" do
+      it { "foo { }".should_extract_to [] }
+    end
+    context "with simple block(do ... end) without args" do
+      it { "foo { }".should_extract_to [] }
+    end
+    context "with simple block(do ... end) parameter" do
+      it { "foo do|a|; end".should_extract_to [["a", 1, 7]] }
+    end
+    context "with lvar reference in block body" do
+      it { "foo {|x| x}".should_extract_to [["x", 1, 6], ["x", 1, 9]] }
+    end
+
+    context "bare lvar reference" do
+      it { "x = 1; y; x".should_extract_to [["x", 1, 0], ["x", 1, 10]] }
+    end
+
+    context "lvar reference in method call lhs"
+    context "lvar reference in method call argument"
+    context "lvar reference in block"
+    context "lvar reference in method body"
+    context "lvar reference in method class body"
+    context "lvar reference in method module body"
+    context "method reference in expr should ignored"
+
+    it "with complex block parameter like {|a, (b, c)| }"
+    it "with multi assignment like a = b = c"
+    it "with assignment in rhs"
+    it "with assignment in method call"
 
     context "with method definition" do
       it "with simple method parameter"
