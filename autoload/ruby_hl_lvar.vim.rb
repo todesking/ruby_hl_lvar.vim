a71edd5882508d0b5a4ee78b7459e0676c4ad6fe
todesking <discommunicative@gmail.com>
2014-04-21 01:20:49 +0900

    matcher cache

--- autoload/ruby_hl_lvar.vim.rb
+++ autoload/ruby_hl_lvar.vim.rb
@@ -31,6 +31,10 @@ end
 
 module RubyHlLvar
   class Extractor
+    def initialize
+      # rule_name => Patm::Rule
+      @rules = {}
+    end
     # source:String -> [ [lvar_name:String, line:Numeric, col:Numeric]... ]
     def extract(source)
       sexp = Ripper.sexp(source)
@@ -105,18 +109,78 @@ module RubyHlLvar
       end
     end
 
-    def extract_from_sexp(sexp)
-      root_matcher.apply(sexp)
+    def extract_from_sexp(obj)
+      match(:root, obj) do|r|
+        p = Patm
+        _any = p::ANY
+        _xs = p::ARRAY_REST
+
+        _1 = p._1
+        _2 = p._2
+        _3 = p._3
+        _4 = p._4
+
+        # single assignment
+        r.on [:assign, [:var_field, [:@ident, _1, [_2, _3]]], _4] do|m|
+          [[m._1, m._2, m._3]] + extract_from_sexp(m._4)
+        end
+        # mass assignment
+        r.on [:massign, _1, _2] do|m|
+          handle_massign_lhs(m._1) + extract_from_sexp(m._2)
+        end
+        # local variable reference
+        r.on [:var_ref, [:@ident, _1, [_2, _3]]] do|m|
+          [[m._1, m._2, m._3]]
+        end
+        # method params
+        r.on [:params, _1, _2, _3, _xs] do|m|
+          handle_normal_params(m._1) + handle_default_params(m._2) + handle_rest_param(m._3)
+        end
+        r.on p.or(nil, true, false, Numeric, String, Symbol, []) do|m|
+          []
+        end
+        r.else do|sexp|
+          if sexp.is_a?(Array) && sexp.size > 0
+            if sexp[0].is_a?(Symbol) # some struct
+              sexp[1..-1].flat_map {|elm| extract_from_sexp(elm) }
+            else
+              sexp.flat_map{|elm| extract_from_sexp(elm) }
+            end
+          else
+            puts "WARN: Unsupported AST data: #{sexp.inspect}"
+            []
+          end
+        end
+      end
     end
 
     private
+      def match(rule_name, obj, &rule)
+        (@rules[rule_name] ||= ::Patm::Rule.new(&rule)).apply(obj)
+      end
       def handle_massign_lhs(lhs)
         return [] unless lhs
         if lhs.size > 0 && lhs[0].is_a?(Symbol)
           lhs = [lhs]
         end
-        matcher = massign_lhs_matcher
-        lhs.flat_map {|expr| matcher.apply expr }
+        lhs.flat_map {|expr|
+          match(:massign_lhs_item, expr) do|r|
+            p = Patm
+            r.on [:@ident, p._1, [p._2, p._3]] do|m|
+              [[m._1, m._2, m._3]]
+            end
+            r.on [:mlhs_paren, p._1] do|m|
+              handle_massign_lhs(m._1)
+            end
+            r.on [:mlhs_add_star, p._1, p._2] do|m|
+              handle_massign_lhs(m._1) + handle_massign_lhs([m._2])
+            end
+            r.else do|obj|
+              puts "WARN: Unsupported ast item in handle_massign_lhs: #{obj.inspect}"
+              []
+            end
+          end
+        }
       end
 
       def handle_normal_params(list)
@@ -124,15 +188,18 @@ module RubyHlLvar
       end
 
       def handle_rest_param(rest)
-        p = Patm
-        case rest
-        when m = p.match([:rest_param, [:@ident, p._1, [p._2, p._3]]])
-          [[m._1, m._2, m._3]]
-        when nil
-          []
-        else
-          puts "WARN: Unsupported ast item in handle_rest_params: #{rest.inspect}"
-          []
+        match(:rest_param, rest) do|r|
+          p = Patm
+          r.on [:rest_param, [:@ident, p._1, [p._2, p._3]]] do|m|
+            [[m._1, m._2, m._3]]
+          end
+          r.on nil do
+            []
+          end
+          r.else do|obj|
+            puts "WARN: Unsupported ast item in handle_rest_params: #{obj.inspect}"
+            []
+          end
         end
       end
 
@@ -141,11 +208,13 @@ module RubyHlLvar
         p = Patm
         _any = p::ANY
         list.flat_map {|expr|
-          case expr
-          when m = p.match([[:@ident, p._1, [p._2, p._3]], _any])
-            [[m._1, m._2, m._3]]
-          else
-            []
+          match(:default_param, expr) do|r|
+            r.on [[:@ident, p._1, [p._2, p._3]], _any] do|m|
+              [[m._1, m._2, m._3]]
+            end
+            r.else do
+              []
+            end
           end
         }
       end
